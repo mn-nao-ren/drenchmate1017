93df767bcced79718d95b14ee31fae6d8ed4c722
mn-nao-ren <junehong.tan@gmail.com>
2024-07-01 14:18:48 +0800

    dashboard redesign

---
+++ lib/presentation/screens/dashboard_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:drenchmate_2024/presentation/components/highlights_grid.dart';
+
+import 'package:drenchmate_2024/presentation/components/dashboard_appbar.dart';
+import 'package:drenchmate_2024/presentation/components/dashboard_header.dart';
+import 'package:drenchmate_2024/presentation/components/header.dart';
+import 'package:drenchmate_2024/presentation/components/shortcuts_grid.dart';
+
+class DashboardView extends StatefulWidget {
+  const DashboardView({super.key});
+
+  @override
+  State<DashboardView> createState() => DashboardController();
+}
+class DashboardController extends State<DashboardView> {
+  final PageController pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: const DashboardAppBar(),
+        body: ListView(
+            children: [
+              DashboardHeader(pageController: pageController),
+              const SizedBox(height: 15),
+              const Header(headerTitle: 'Highlights'),
+              const SizedBox(height: 4),
+              const HighlightsGrid(),
+              const SizedBox(height: 4),
+              const Header(headerTitle: 'ShortCuts'),
+              const ShortcutsGrid(),
+
+            ],
+        ),
+    );
+  }
+}
